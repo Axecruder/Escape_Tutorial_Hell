a379aedc8a7303bbc0a6d9b27111f05e2e85ccde
Debreceni Marcell <debreceni.marci@gmail.com>
2020-05-15 19:57:14 +0200

    Menü

---
+++ Escape Tutorial Hell/Assets/Scipts/UI/PauseGameButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGameButton : MonoBehaviour
+{
+    public Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadMap()
+    {
+        SaveSystem.SavePlayer(player);
+        SceneManager.LoadScene("LaunchScene", LoadSceneMode.Single);
+    }
+}

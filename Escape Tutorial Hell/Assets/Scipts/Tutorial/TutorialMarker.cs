e8df9f9c2ef13c1b2a33bdd207fb0e6580450ac3
Debreceni Marcell <debreceni.marci@gmail.com>
2020-05-17 19:21:52 +0200

    UI changes camera changes

---
+++ Escape Tutorial Hell/Assets/Scipts/Tutorial/TutorialMarker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialMarker : MonoBehaviour
+{
+    public string message;
+    public GameObject Tutorialbox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Tutorialbox.SetActive(true);
+            Text messagebox = GameObject.Find("TutorialText").GetComponent<Text>();
+            messagebox.text = message;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Tutorialbox.SetActive(false);
+        }
+    }
+}
